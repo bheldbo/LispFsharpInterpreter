91f54b2e4245416f1034e258fcd321eab6a9d023
Bjarke Heldbo <bjarke@heldbo.net>
2019-03-28 02:17:00 +0100

    init lisp interpreter

---
+++ RunLISP.fsx
@@ -0,0 +1,179 @@
+
+// REPL for LISP variant
+// Compile with "source compile.sh"
+// Run with "mono lisp.exe"
+
+// Type in S-expression at the "> " prompt.
+// It may span several lines -- a new prompt is shown for each line.
+// When (after an "Enter" is typed) a complete S-expression is found
+// it is evaluated and the result shown after a "= ".
+// If an error is found during parsing or evaluation,
+// a message is displayed after a "! ".
+// In both cases, the "> " prompt is shown again.
+// Close session by pressing ^D (control-D) at the prompt
+
+open Sexp;;
+
+// Global environment (initially empty)
+
+let mutable globalEnv = []
+
+// look up variable in environment
+// environment is represented as list of (name,value) pairs
+
+let rec lookup env x =
+  match env with
+  | [] -> None
+  | ((y,v) :: env1) -> if x=y then Some v else lookup env1 x
+
+// update binding in environment, adding new if not bound
+// only used for global environment
+
+let rec update env x v =
+  match env with
+  | [] -> [(x,v)]
+  | ((y,w) :: env1) ->
+       if x=y then (x,v) :: env1 else (y,w) :: update env1 x v
+
+// update global environment
+
+let updateGlobal x v =
+  globalEnv <- update globalEnv x v
+
+// exception for LISP evaluation errors
+
+exception Lerror of string
+
+// list of keywords
+let keywords = ["quote"; "lambda"; "define"; "cons"; "save"; "load"; "+";]
+
+// evaluate Sexp to Sexp in local environment
+// if error raise exception Lerror message
+
+let rec eval s localEnv =
+  match s with
+  | Nil -> Nil
+  | Symbol x -> // keyword or variable
+      if List.contains x keywords then
+        raise (Lerror ("keyword " + x + " can not be used as variable"))
+      else
+        match lookup (localEnv @ globalEnv) x with
+        | Some v -> v
+        | None -> raise (Lerror ("Undefined variable " + x))
+  | Number value -> Number value
+  | Cons (Symbol "quote", Cons (v, Nil)) -> v
+  | Cons (Symbol "lambda", rules) -> s
+  | Cons (Symbol "define", Cons (Symbol x, Cons (e, Nil))) ->
+      if List.contains x keywords then
+        raise (Lerror ("keyword " + x + " can not be redefined"))
+      else
+        (updateGlobal x (eval e localEnv); Nil)
+  | Cons (Symbol "cons", Cons (e1, Cons (e2, Nil))) ->
+      Cons (eval e1 localEnv, eval e2 localEnv)
+  | Cons (Symbol "save", Cons (Symbol f, Nil)) ->
+      try
+        let envText = // create definitions as text, one per line
+              String.concat "\n"
+                (List.map
+                  (fun (x,v) -> "(define " + x + " " + quoteExp v + ")")
+                   globalEnv)
+        let outfile = System.IO.File.CreateText (f + ".le")
+        outfile.Write envText; outfile.Close (); Nil
+      with _ -> raise (Lerror ("Could not open file " + f + ".le"))
+  | Cons (Symbol "load", Cons (Symbol f, Nil)) ->
+      try
+        let infile = System.IO.File.OpenText (f + ".le")
+        while not infile.EndOfStream do // read definitions, one per line
+          match readSexp (infile.ReadLine ()) with
+          | Success (s, 0) -> ignore (eval s [])
+          | _ -> raise (Lerror ("Error while reading " + f + ".le"))
+        ; infile.Close (); Nil
+      with _ -> raise (Lerror ("Could not open file " + f + ".le"))
+  | Cons (Symbol x, pars) when List.contains x keywords ->
+      raise (Lerror ("malformed " + x + " expression"))
+  | Cons (e1, pars) -> // function application
+      match eval e1 localEnv with
+      | Cons (Symbol "lambda", rules) ->
+          tryRules rules (evalList pars localEnv) localEnv
+      | v -> raise (Lerror (showSexp v + " can not be applied as a function"))
+
+
+// evaluate argument list
+
+and evalList es localEnv =
+  match es with
+  | Nil -> Nil
+  | Cons (e1, es1) -> Cons (eval e1 localEnv, evalList es1 localEnv)
+  | _ -> raise (Lerror ("arguments are not a list: " + showSexp es))
+
+and tryRules rs args localEnv =
+  match rs with
+  | Cons (p, Cons (e, rs1)) ->
+      match matchPattern p args with
+      | Some env -> eval e (env @ localEnv)
+      | None -> tryRules rs1 args localEnv
+  | Nil -> raise (Lerror ("No patterns matched arguments " + showSexp args))
+  | _ ->  raise (Lerror ("Malformed rules " + showSexp rs))
+
+// match pattern to argument list
+
+and matchPattern p v =
+  match (p, v) with
+  | (Nil, Nil) -> Some []
+  | (Symbol x, w) ->
+       if List.contains x keywords
+       then raise (Lerror ("keyword " + x + " can not be used in pattern"))
+       else Some [(x, w)]
+  | (Cons (p1, p2), Cons (v1, v2)) ->
+       match (matchPattern p1 v1, matchPattern p2 v2) with
+       | (Some env1, Some env2) ->
+            if disjoint env1 env2 then Some (env1 @ env2) else None
+       | _ -> None
+  | _ -> None
+
+// check if variable is repeated in pattern by comparing environments
+
+and disjoint env1 env2 =
+  match env1 with
+  | [] -> true
+  | (x,v) :: env3 ->
+       match lookup env2 x with
+       | Some _ -> raise (Lerror ("repeated variable " + x + " in pattern"))
+       | None -> disjoint env3 env2
+
+and quoteExp v =
+  match v with
+  | Nil -> "()"
+  | (Cons (Symbol "quote", _)) -> showSexp v
+  | (Cons (Symbol "lambda", _)) -> showSexp v
+  | _ -> "(quote " + showSexp v + ")"
+
+// read-eval-print loop (REPL) for LISP variant
+// See functionality at top of this file
+
+exception Exit
+
+let rec repl prefix =
+  if prefix = "" then printf "> " ;
+  let input = System.Console.ReadLine()
+  if input = null then raise Exit
+  let s = prefix + input
+  let len = String.length s
+  match Sexp.readSexp s with
+  | Success (e, p) ->
+     if p=0 then
+       (try printf "= %s\n" (showSexp (eval e []))
+        with Lerror mess -> printf "! %s \n" mess)
+     else
+       printf "! %s\n" "Input is not a single S-expression"
+     ; repl ""
+  | ErrorAt i ->
+      if i=len then
+        repl s
+      else
+       printf "! %s \n" ("parse error at position " + string i);
+       repl ""
+
+// Start REPL
+
+printf "PLD LISP v. 1.0\n"; try repl "" with Exit -> printf "\n"
---
+++ Sexp.fs
@@ -0,0 +1,160 @@
+// Sexp.fs: S-expressions for LISP variant
+
+module Sexp
+
+// Type for S-expressions
+
+type Sexp = Symbol of string
+          | Nil
+          | Cons of Sexp * Sexp
+          | Number of int // Integers
+
+// Convert Sexp to string
+
+let rec showSexp s =
+  match s with
+  | Symbol name -> name
+  | Nil  -> "()"
+  | Cons (Symbol "quote", Cons (s, Nil)) -> "'" + showSexp s
+  | Cons (a,d) -> "(" + showSexp a + showTail d
+  | Number value -> (string value) // Integers
+
+
+
+and showTail d =
+  match d with
+  | Symbol name -> " . " + name + ")"
+  | Nil  -> ")"
+  | Cons (a,d1) -> " " + showSexp a + showTail d1
+
+
+// Type for success or failure for parsing
+
+type ParseResult = Success of Sexp * int // expression and position
+                 | ErrorAt of int // position
+
+
+// Read Sexp from string
+// Return Success (s, 0) if input matches an Sexp s with no "junk" afterwards
+// Return Success (s, i) if prefix of input matches,
+// but there is "junk" at position i
+// Return ErrorAt i if a parse error is found at position i
+
+let rec readSexp cs =
+  let len = String.length cs
+  match readExp cs 0 len with
+  | Success (s, i) ->
+      let j = skipWhite cs i len
+      if j=len then Success (s, 0) else Success (s, j)
+  | ErrorAt i -> ErrorAt i
+
+// skip whitespace
+
+and skipWhite cs i len  =
+  if i < len && (cs.[i]  = ' ' || cs.[i] = '\n')
+  then skipWhite cs (i+1) len
+  else i
+
+// read Sexp from position i in string
+// and return either ErrorAt j (if string didn't match at position j) or
+// Success (s, j) where s is the read Sexp and j is position after read Sexp
+
+and readExp cs i len =
+  if i = len then ErrorAt i
+  else
+    let c = cs.[i]
+    if 'a' <= c &&  c <= 'z' then // read symbol
+      readSymbol (string c) cs (i+1) len
+    else if '0' <= c && c <= '9' then // Integers
+      readInt (int c) cs (i+1) len
+    else if c = '(' then // this starts a list or Nil, read the rest
+      readTail cs (i+1) len
+    else if c = '\'' then // read quoted expression
+      match readExp cs (i+1) len with
+      | Success (s, j) -> Success (Cons (Symbol "quote", Cons (s, Nil)), j)
+      | other -> other
+    else if c = ' ' || c = '\n' then // skip whitespace
+      readExp cs (i+1) len
+    else ErrorAt i
+
+// Integers
+// read int
+// add number to exp (already read number)
+// stop when end of string or non-letter reached
+
+and readInt value cs i len =
+  if i = len then Success (Number value, i)
+  else
+    let c = cs.[i]
+    if '0' <= c && c <= '9' then
+      readInt ((value-48)*10 + (int c)) cs (i+1) len
+    else Success (Number (value-48), i)
+
+// read symbol (all lower case letters)
+// add letters to name (already read letters)
+// stop when end of string or non-letter reached
+
+and readSymbol name cs i len =
+  if i = len then Success (Symbol name, i)
+  else
+    let c = cs.[i]
+    if 'a' <= c &&  c <= 'z' then readSymbol (name + string c) cs (i+1) len
+    else Success (Symbol name, i)
+
+// read list tail
+// handles shorthand (a1 a2 ... an) = (a1 . (a2 . ... an))
+
+and readTail cs i len  =
+  if i = len then ErrorAt i
+  else
+    let c = cs.[i]
+    if c = ')' then Success (Nil, i+1)  // end of list
+    else if 'a' <= c &&  c <= 'z' then  // read symbol and then tail
+      match readSymbol (string c) cs (i+1) len with
+      | ErrorAt j -> ErrorAt j
+      | Success (sym, j) ->
+         match readTail cs j len with
+         | ErrorAt j -> ErrorAt j
+         | Success (list, j) -> Success (Cons (sym, list), j)
+    else if c = '(' then // read Sexp and then tail
+      match readTail cs (i+1) len with
+      | ErrorAt j -> ErrorAt j
+      | Success (s, j) ->
+         match readTail cs j len with
+         | ErrorAt j -> ErrorAt j
+         | Success (list, j) -> Success (Cons (s, list), j)
+    else if c = '\'' then // read 'Sexp and then tail
+      match readExp cs (i+1) len with
+      | Success (s, j) ->
+         match readTail cs j len with
+         | ErrorAt j -> ErrorAt j
+         | Success (list, j) ->
+             Success (Cons (Cons (Symbol "quote", Cons (s, Nil)), list), j)
+      | other -> other
+    else if c = '.' then // read Sexp and then ")"
+      match readExp cs (i+1) len with
+      | ErrorAt j -> ErrorAt j
+      | Success (s, j) ->
+         match readClose cs j len with
+         | ErrorAt j -> ErrorAt j
+         | Success (_, j) -> Success (s, j)
+    else if c = ' ' || c = '\n' then // skip whitespace
+      readTail cs (i+1) len
+    else if '0' <= c &&  c <= '9' then  // Integers read number and then tail
+      match readInt (int c) cs (i+1) len with
+      | ErrorAt j -> ErrorAt j
+      | Success (sym, j) ->
+         match readTail cs j len with
+         | ErrorAt j -> ErrorAt j
+         | Success (list, j) -> Success (Cons (sym, list), j)
+    else ErrorAt i
+
+// read close parenthesis (possibly preceeded by whitespace)
+
+and readClose cs i len  =
+  if i = len then ErrorAt i
+  else
+    let c = cs.[i]
+    if c = ')' then Success (Nil, i+1)
+    else if c = ' ' || c = '\n' then readClose cs (i+1) len
+    else ErrorAt i
